5bb38a508a1ec51deee579e1e8b37946839ffe78
VarshiniBR <87134759+VarshiniBR@users.noreply.github.com>
2022-07-02 12:38:14 +0530

    Add files via upload

---
+++ Backend/wipro-ecom-rest-api/src/main/java/com/example/demo/MedicineEcomRestApiApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class MedicineEcomRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineEcomRestApiApplication.class, args);
+	}
+
+}
