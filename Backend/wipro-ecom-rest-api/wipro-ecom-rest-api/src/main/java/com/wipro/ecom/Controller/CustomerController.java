97de58f12a48312c2e12d1cf8a7e39311ab02019
kavya-kommu <71807092+kavya-kommu@users.noreply.github.com>
2022-07-02 13:59:22 +0530

    Add files via upload

---
+++ Backend/wipro-ecom-rest-api/wipro-ecom-rest-api/src/main/java/com/wipro/ecom/Controller/CustomerController.java
@@ -0,0 +1,53 @@
+package com.wipro.ecom.Controller;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wipro.ecom.Service.CustomerService;
+import com.wipro.ecom.entity.Customer;
+
+
+
+@RequestMapping("/customer")
+@CrossOrigin(origins = {"http://localhost:4200"})
+@RestController
+public class CustomerController {
+
+	@Autowired
+	private CustomerService cs;
+	
+	@PostMapping
+	public Customer registerCustomer(@RequestBody Customer customer)
+	{
+		return cs.create(customer);
+	}
+	@GetMapping
+	public List<Customer> findAllCustomers()
+	{
+		return cs.read();
+	}
+	@PutMapping
+	public Customer updateCustomer(@RequestBody Customer customer)
+	{
+		return cs.update(customer);
+	}
+	@DeleteMapping("/{id}")
+	public Customer deleteCustomer(@PathVariable("id") Integer id)
+	{
+		return cs.delete(id);
+	}
+	
+	
+	
+}
---
+++ Backend/wipro-ecom-rest-api/wipro-ecom-rest-api/src/main/java/com/wipro/ecom/WiproEcomRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiproEcomRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiproEcomRestApiApplication.class, args);
+	}
+
+}
